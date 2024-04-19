2d6e12a4aa3aa1f5671183f11f6de5f6c1a28850
Chinmaya.0patra <chinmay.0patra@gmail.com>
2024-04-19 09:40:50 +0530

    Add project files.

---
+++ UserRegisterApp/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace UserRegisterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.btnReg = new System.Windows.Forms.Button();
+            this.btnLog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFname
+            // 
+            this.txtFname.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFname.Location = new System.Drawing.Point(237, 117);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(270, 26);
+            this.txtFname.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 121);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(588, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Last Name :";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLname.Location = new System.Drawing.Point(714, 117);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(270, 26);
+            this.txtLname.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(111, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "DOB :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(111, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(109, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "User Name :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(588, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 22);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Phone :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(588, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 22);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Gender :";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(111, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 22);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Email :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(111, 434);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 22);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Country :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(588, 365);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 22);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Password :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Bell MT", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(588, 434);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(0, 22);
+            this.label10.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(237, 206);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(270, 26);
+            this.dateTimePicker1.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MALE",
+            "FEMALE",
+            "OTHERS"});
+            this.comboBox1.Location = new System.Drawing.Point(714, 210);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(270, 28);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // txtMail
+            // 
+            this.txtMail.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMail.Location = new System.Drawing.Point(237, 294);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(270, 26);
+            this.txtMail.TabIndex = 14;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPhone.Location = new System.Drawing.Point(714, 298);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(270, 26);
+            this.txtPhone.TabIndex = 15;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsername.Location = new System.Drawing.Point(237, 361);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(270, 26);
+            this.txtUsername.TabIndex = 16;
+            // 
+            // txtPass
+            // 
+            this.txtPass.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPass.Location = new System.Drawing.Point(714, 361);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.Size = new System.Drawing.Size(270, 26);
+            this.txtPass.TabIndex = 17;
+            this.txtPass.UseSystemPasswordChar = true;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountry.Location = new System.Drawing.Point(237, 430);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(270, 26);
+            this.txtCountry.TabIndex = 18;
+            // 
+            // btnReg
+            // 
+            this.btnReg.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReg.Location = new System.Drawing.Point(513, 513);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(98, 40);
+            this.btnReg.TabIndex = 19;
+            this.btnReg.Text = "Register";
+            this.btnReg.UseVisualStyleBackColor = true;
+            this.btnReg.Click += new System.EventHandler(this.btnReg_Click);
+            // 
+            // btnLog
+            // 
+            this.btnLog.Font = new System.Drawing.Font("Bell MT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLog.Location = new System.Drawing.Point(660, 513);
+            this.btnLog.Name = "btnLog";
+            this.btnLog.Size = new System.Drawing.Size(98, 40);
+            this.btnLog.TabIndex = 20;
+            this.btnLog.Text = "Login";
+            this.btnLog.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(1093, 618);
+            this.Controls.Add(this.btnLog);
+            this.Controls.Add(this.btnReg);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtPass);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFname);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Button btnReg;
+        private System.Windows.Forms.Button btnLog;
+    }
+}
+
---
+++ UserRegisterApp/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace UserRegisterApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReg_Click(object sender, EventArgs e)
+        {
+
+            emptyChcker();
+
+            
+
+
+        }
+
+        private void emptyChcker()
+        {
+            bool isEmpty = false;
+            foreach(Control control in this.Controls)
+            {
+                if(control is TextBox)
+                {
+                    if (string.IsNullOrEmpty(control.Text))
+                    {
+                        isEmpty = true;
+                        break;
+                    }
+                }
+                else if(control is DateTimePicker)
+                {
+                    if(((DateTimePicker)control).Value == null)
+                    {
+                        isEmpty = true;
+                        break;
+                    }
+                }
+                else if(control is ComboBox) 
+                {
+                    if(((ComboBox)control).SelectedIndex == -1)
+                    {
+                        isEmpty = true;
+                        break;
+                    }
+                }
+            }
+            if( isEmpty )
+            {
+                MessageBox.Show("Fields can not be blank! Enter Values and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection("Data Source=CHINMAYA-PATRA\\SQLEXPRESS;Initial Catalog=RegisterForm;Integrated Security=True;TrustServerCertificate=True");
+                con.Open();
+                string insertQ = "INSERT INTO UserData VAlUES(@fname, @lname, @dob, @gender, @mail, @username, @pass, @phone, @country)";
+                SqlCommand cmd = new SqlCommand(insertQ, con);
+                cmd.Parameters.AddWithValue("@fname", txtFname.Text);
+                cmd.Parameters.AddWithValue("@lname", txtLname.Text);
+                cmd.Parameters.AddWithValue("@dob", dateTimePicker1.Text);
+                cmd.Parameters.AddWithValue("@gender", comboBox1.Text);
+                cmd.Parameters.AddWithValue("@mail", txtMail.Text);
+                cmd.Parameters.AddWithValue("@phone", txtPhone.Text);
+                cmd.Parameters.AddWithValue("@username", txtUsername.Text);
+                cmd.Parameters.AddWithValue("@pass", txtPass.Text);
+                cmd.Parameters.AddWithValue("@country", txtCountry.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Registration Successful!", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
